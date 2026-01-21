0919e84ce7e88e0c2c36e9e268b34c6effe84641
Adrian Chadd <adrian@FreeBSD.org>
2026-01-20 22:47:53 -0800

    toolchain: hack to get genie to run through no android toolchain for bsd
    this is definitely a hack; since there's only currently a "bsd"
    target now.
    
    it's likely worth creating multiple bsd targets and then we can have
    multiple targets here..

--- scripts/toolchain.lua
+++ scripts/toolchain.lua
@@ -60,7 +60,9 @@ function androidToolchainRoot()
 		local hostTags = {
 			windows = "windows-x86_64",
 			linux   = "linux-x86_64",
-			macosx  = "darwin-x86_64"
+			macosx  = "darwin-x86_64",
+			-- placeholder so things will actually build!
+			bsd     = "bsd-x86_64"
 		}
 		android.toolchainRoot = (os.getenv("ANDROID_NDK_HOME") or "") .. "/toolchains/llvm/prebuilt/" .. hostTags[os.get()]
 	end
